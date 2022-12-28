571b9ffa8f6ab301fa0eae78d9c972ae96a9c218
EasoncoderTW <96429799+EasoncoderTW@users.noreply.github.com>
2022-12-28 19:44:04 +0800

    Update ALU.v

--- src/ALU.v
+++ src/ALU.v
@@ -1,30 +1,4 @@
-//fun3
-`define add_sub 3'b000
-`define sll_    3'b001
-`define slt_    3'b010
-`define sltu_   3'b011
-`define xor_    3'b100
-`define srl_sra 3'b101
-`define or_     3'b110
-`define and_    3'b111
-
-`define beq_    3'b000
-`define bne_    3'b001
-`define blt_    3'b100
-`define bge_    3'b101
-`define bltu_   3'b110
-`define bgeu_   3'b111
-
-// opcode
-`define lui_    5'b01101
-`define auipc_  5'b00101
-`define load_   5'b00000
-`define store_  5'b01000
-`define jal_    5'b11011
-`define jalr_   5'b11001
-`define branch_   5'b11000
-`define I_type_   5'b00100
-`define R_type_   5'b01100
+`include "rv32_define.v"
 
 module ALU (
     input [4:0] opcode,
