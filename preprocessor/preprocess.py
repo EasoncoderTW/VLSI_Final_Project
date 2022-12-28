de60d8f836b8b210d63aba7be881b7b8a7085d97
fennecJ <hwahwa649@gmail.com>
2022-12-28 15:56:40 +0800

    Supports ret conversion
    ret -> jalr x0, ra

--- preprocessor/preprocess.py
+++ preprocessor/preprocess.py
@@ -13,25 +13,25 @@ def parse(line):
     line = line.strip('\n')
     res = line.split('#', 1)
     str = ''
+    code = res[0].rstrip()
+    if code == 'ret':
+        code = 'jalr x0, 0(ra)'
     if(len(res) == 2): # contains '#' in line
-        code = res[0]
         comments = '#' + res[1]
         str = ' ' * leadSpaceLen + comments
         if(code): # comment + code
             str += '\n'
             str += ' ' * leadSpaceLen + code
             # remove trailing spaces
-            str = str.rstrip()
             # str += '\n' # Minor improve readability
 
     else:   # code only
-        code = res[0]
         str = ' ' * leadSpaceLen + code
     return str
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print(f'Usage: ./{sys.argv[0]} $fileIn $fileOut')
+        print(f'Usage: python3 ./{sys.argv[0]} $fileIn $fileOut')
         sys.exit()
     fi = open(sys.argv[1], 'r')
     fo = open(sys.argv[2], 'w')
