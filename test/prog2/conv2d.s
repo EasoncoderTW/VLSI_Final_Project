e96cd155f566f1408a979d41b121027aad8a3009
TWstinkytofu <a3012085@icloud.com>
2023-01-01 19:43:56 +0800

    add conv2d/merge conv2d.s into main.s in prog2

--- test/prog2/conv2d.s
+++ test/prog2/conv2d.s
@@ -1,4 +1,4 @@
-#reg use s0~11,ra
+#reg use s1,s2,s4~11,ra
 .data
 IFM: .word 42,5,62,23,20,4,72,57,76,34,42,12,68,76,8,27,38,50,8,65,64,46,57,75,18
 WEIGHT: .word 22,38,56,40,25,33,55,40,34
@@ -11,105 +11,104 @@ KER_SIZE: .word 3
 #both integer and weight is less than 256
 conv2d:
     #a5 p_sum(conv)
-    addi  sp, sp ,-4
-    sw x1, 0(sp)
+    #s1 change of addr when x add 1(IFM)
+    #s2 change of addr when x add 1(WEI)
+    addi  sp, sp ,-44
+    sw ra, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s4, 12(sp)
+    sw s5, 16(sp)
+    sw s6, 20(sp)
+    sw s7, 24(sp)
+    sw s8, 28(sp)
+    sw s9, 32(sp)
+    sw s10, 36(sp)
+    sw s11, 40(sp)
     la t1 IFM_SIZE
     lw t1 0(t1)
-    li t3 0xB000
-    #without padding =>-2
+    add a1 x0 t1
     addi t1 t1 -2
+    #without padding =>-2
+    #input change
+    li a2 4
+        #a word = 4 bytes
+    jal x1 multi
+    add s1 x0 a0
+    #weight change
+    la a1 KER_SIZE
+    lw a1 0(a1)
+    li a2 4
+    jal x1 multi
+    add s2 x0 a0
+    #store result at B000
+    li t3 0xB000
+    #s4 base addr for some x(IFM)
+    la s4 IFM
 conv2d_loop_IFM_xaxis:
     #t1 x_loc of IFM
     #t2 y_loc of_IFM
     #t3 store addr 
+    #s5 base addr for some y(IFM)
     la t2 IFM_SIZE
     lw t2 0(t2)
     addi t2 t2 -2
+    #without padding =>-2
+    add s5 x0 s4
     jal x0 conv2d_loop_IFM_yaxis
 IFM_x_keep:
     addi t1 t1 -1
+    add s4 s4 s1
     bne t1 x0 conv2d_loop_IFM_xaxis
-    jal x0 exit
+    jal x0 exit_conv2d
 
 conv2d_loop_IFM_yaxis:
     #s10 WEI x_addr
+    #a5 p_sum
+    #s6 base addr for some x,y(IFM)
+    #s8 base addr for some x(KER)
     la s10 KER_SIZE
     lw s10 0(s10)
     la t0 IFM
     li a5 0
+    add s6 x0 s5
+    la s8 WEIGHT
     jal x0 conv2d_loop_WEI_xaxis
 IFM_y_keep:
     addi t2 t2 -1
     sw a5 0(t3)
+    addi t3 t3 4
+    addi s5 s5 4
     bne t2 x0 conv2d_loop_IFM_yaxis
     jal x0 IFM_x_keep
 
 conv2d_loop_WEI_xaxis:
     #s11 WEI y_addr
+    #s7 base addr for some x,y(IFM) and x(KERNEL) - IFM
+    #s9 base addr for some x(KERNEL) - KER
     la s11 KER_SIZE
     lw s11 0(s11)
+    add s7 x0 s6
+    add s9 x0 s8
     jal x0 conv2d_loop_WEI_yaxis
 WEI_x_keep:
     addi s10 s10 -1
+    add s6 s6 s1
+    add s8 s8 s2
     bne s10 x0 conv2d_loop_WEI_xaxis
     jal x0 IFM_y_keep
 
 conv2d_loop_WEI_yaxis:
-    #s0 addr offset
-    li s0 0
-    la t0 IFM
-    #load input
-    jal x0 load_input
-load_input_fin:
-    #s3 weight    
-    add t0 t0 s0 
-    lw t0 0(t0)
-    #load weight
-    la a2 KER_SIZE
-    jal x0 load_input
-load_weight_fin:    
-    add s3 s3 s0
-    lw s3 0(t0)
-    addi a1 t0 0
-    addi a2 s3 0
+    
+    lw a1 0(s7)
+    lw a2 0(s9)
     jal x1 multi
-    addi s11 s11 -1
     add a5 a5 a0
+    addi s11 s11 -1
+    addi s7 s7 4
+    addi s9 s9 4
     bne s11 x0 conv2d_loop_WEI_yaxis
     jal x0 WEI_x_keep
-
-load_input:
-    #loc_x
-    #ini a2 =>size addr
-    add a1 x0 t1  
-    la a2 IFM_SIZE
-    lw a2 0(a2)
-    slli a2 a2 2
-    jal  x1 multi
-    add s0 s0 a0
-    #loc_y
-    add a1 x0 t2
-    li a2 4
-    jal x1 multi
-    add s0 s0 a0
-    jal x0 load_weight_fin
-
-
-load_weight:
-    #loc_x
-    #ini a2 =>size addr
-    add a1 x0 t1  
-    la a2 KER_SIZE
-    lw a2 0(a2)
-    slli a2 a2 2
-    jal  x1 multi
-    add s0 s0 a0
-    #loc_y
-    add a1 x0 t2
-    li a2 4
-    jal x1 multi
-    add s0 s0 a0
-    jal x0 load_input_fin
 
 multi:
     #a1 multiplicand
@@ -123,18 +122,29 @@ loop_multi:
     andi a6 a2 128
     bne a6 x0 add_to_psum 
 keep_mul:
+    addi a4 a4 -1  
     slli a2 a2 1 
-    slli a0 a0 1
-    addi a4 a4 -1    
-    beq a4 x0 loop_multi
+    slli a0 a0 1  
+    bne a4 x0 loop_multi
+    srli a0 a0 1
     ret
 add_to_psum:
     add a0 a0 a1
     jal x0 keep_mul
 
 
-exit:
-    jal halt
-
-halt:
+exit_conv2d:
+    ##restore reg 
+    lw ra, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s4, 12(sp)
+    lw s5, 16(sp)
+    lw s6, 20(sp)
+    lw s7, 24(sp)
+    lw s8, 28(sp)
+    lw s9, 32(sp)
+    lw s10, 36(sp)
+    lw s11, 40(sp)
+    addi sp, sp ,-44
     ret
--- test/prog2/main.s
+++ test/prog2/main.s
@@ -1,11 +1,17 @@
 .data
-test1: .word 16,2,4,16,4,10,12,2,14,8,4,14,6,4,2,10,12,6,10,2,14,14,6,8,16,8,16,6,12,10,8,123
-test2: .word 470,405,225,197,126,122,56,33,-81,-275,-379,-409,-416,-496,-500
-test3: .word 412,-474,443,171,-23,247,221,7,40,221,-90,61,-9,49,-80,-80,221,-379,-161,-397,-173,276,-197,221,-12,-145,101
+test1: .word 16 2 4 16 4 10 12 2 14 8 4 14 6 4 2 10 12 6 10 2 14 14 6 8 16 8 16 6 12 10 8 123
+test2: .word 470 405 225 197 126 122 56 33 -81 -275 -379 -409 -416 -496 -500
+test3: .word 412 -474 443 171 -23 247 221 7 40 221 -90 61 -9 49 -80 -80 221 -379 -161 -397 -173 276 -197 221 -12 -145 101
 TEST1_SIZE: .word 32
 TEST2_SIZE: .word 15
 TEST3_SIZE: .word 27
+IFM: .word 42,5,62,23,20,4,72,57,76,34,42,12,68,76,8,27,38,50,8,65,64,46,57,75,18
+WEIGHT: .word 22,38,56,40,25,33,55,40,34
+IFM_SIZE: .word 5
+IFM_NUM: .word 25
+KER_SIZE: .word 3
 _answer: .word 0
+
 
 .text
 
@@ -116,6 +122,7 @@ copy_loop:
     # MEM[@sp-16] -> @s3
     addi  sp, sp, 16
     jal fib_and_instest
+    jal conv2d
     jal x0 main_exit
 
     
@@ -203,7 +210,7 @@ mergesort:
     addi  sp, sp, 12
     
 mergesort_ret:
-    jalr x0, ra, 0
+    jalr x0, 0(ra)
 
 #--------------------#
 #   Function merge   #
@@ -227,7 +234,8 @@ merge:
     add   t1, sp, x0
     
     ### for(int i = 0; i< temp_size;i++)
-    li    t2, 0    # t2: int i
+    # t2: int i
+    li    t2, 0    
     bge   t2, t0, for_loop_1_end
 for_loop_1:
     add   t3, t2, a1    
@@ -289,7 +297,8 @@ while_loop_1:
     lw    t4, 0(t2)
     slli  t2, s4, 2         
     # @arr[arr_index] -> t2
-    add   t2, t2, a0        # word
+    # word
+    add   t2, t2, a0        
     blt   t4, t3, else_1
 if_1:      
     sw    t3, 0(t2)        
@@ -367,7 +376,7 @@ while_loop_3_end:
     add   sp ,sp, t1    
     # @sp = @sp + temp_size
     
-    jalr x0, ra, 0
+    jalr x0, 0(ra)
 
 
 fib_and_instest:
@@ -425,7 +434,7 @@ go_back:
 	addi t6 t6 4
 	#as ==================
 	sw x0 0(t6)
-	jalr x0 x1 0 
+	jalr x0 0(x1)
 
 test_lb_lh_sb_sh_blt:
 	#s0 load pointer
@@ -564,7 +573,146 @@ fib_exit:
 	addi sp sp 8 
     ret
 	
-	
+conv2d:
+    #a5 p_sum(conv)
+    #s1 change of addr when x add 1(IFM)
+    #s2 change of addr when x add 1(WEI)
+    addi  sp, sp ,-44
+    sw ra, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s4, 12(sp)
+    sw s5, 16(sp)
+    sw s6, 20(sp)
+    sw s7, 24(sp)
+    sw s8, 28(sp)
+    sw s9, 32(sp)
+    sw s10, 36(sp)
+    sw s11, 40(sp)
+    la t1 IFM_SIZE
+    lw t1 0(t1)
+    add a1 x0 t1
+    addi t1 t1 -2
+    #without padding =>-2
+    #input change
+    li a2 4
+        #a word = 4 bytes
+    jal x1 multi
+    add s1 x0 a0
+    #weight change
+    la a1 KER_SIZE
+    lw a1 0(a1)
+    li a2 4
+    jal x1 multi
+    add s2 x0 a0
+    #store result at B000
+    li t3 0xB000
+    #s4 base addr for some x(IFM)
+    la s4 IFM
+conv2d_loop_IFM_xaxis:
+    #t1 x_loc of IFM
+    #t2 y_loc of_IFM
+    #t3 store addr 
+    #s5 base addr for some y(IFM)
+    la t2 IFM_SIZE
+    lw t2 0(t2)
+    addi t2 t2 -2
+    #without padding =>-2
+    add s5 x0 s4
+    jal x0 conv2d_loop_IFM_yaxis
+IFM_x_keep:
+    addi t1 t1 -1
+    add s4 s4 s1
+    bne t1 x0 conv2d_loop_IFM_xaxis
+    jal x0 exit_conv2d
+
+conv2d_loop_IFM_yaxis:
+    #s10 WEI x_addr
+    #a5 p_sum
+    #s6 base addr for some x,y(IFM)
+    #s8 base addr for some x(KER)
+    la s10 KER_SIZE
+    lw s10 0(s10)
+    la t0 IFM
+    li a5 0
+    add s6 x0 s5
+    la s8 WEIGHT
+    jal x0 conv2d_loop_WEI_xaxis
+IFM_y_keep:
+    addi t2 t2 -1
+    sw a5 0(t3)
+    addi t3 t3 4
+    addi s5 s5 4
+    bne t2 x0 conv2d_loop_IFM_yaxis
+    jal x0 IFM_x_keep
+
+conv2d_loop_WEI_xaxis:
+    #s11 WEI y_addr
+    #s7 base addr for some x,y(IFM) and x(KERNEL) - IFM
+    #s9 base addr for some x(KERNEL) - KER
+    la s11 KER_SIZE
+    lw s11 0(s11)
+    add s7 x0 s6
+    add s9 x0 s8
+    jal x0 conv2d_loop_WEI_yaxis
+WEI_x_keep:
+    addi s10 s10 -1
+    add s6 s6 s1
+    add s8 s8 s2
+    bne s10 x0 conv2d_loop_WEI_xaxis
+    jal x0 IFM_y_keep
+
+conv2d_loop_WEI_yaxis:
+    
+    lw a1 0(s7)
+    lw a2 0(s9)
+    jal x1 multi
+    add a5 a5 a0
+    addi s11 s11 -1
+    addi s7 s7 4
+    addi s9 s9 4
+    bne s11 x0 conv2d_loop_WEI_yaxis
+    jal x0 WEI_x_keep
+
+multi:
+    #a1 multiplicand
+    #a2 multiplier
+    #a0 p_sum(mul)
+    li a4 8
+    li a0 0
+loop_multi:
+    #do multiply
+    #a4 is counter
+    andi a6 a2 128
+    bne a6 x0 add_to_psum 
+keep_mul:
+    addi a4 a4 -1  
+    slli a2 a2 1 
+    slli a0 a0 1  
+    bne a4 x0 loop_multi
+    srli a0 a0 1
+    ret
+add_to_psum:
+    add a0 a0 a1
+    jal x0 keep_mul
+
+
+exit_conv2d:
+    ##restore reg 
+    lw ra, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s4, 12(sp)
+    lw s5, 16(sp)
+    lw s6, 20(sp)
+    lw s7, 24(sp)
+    lw s8, 28(sp)
+    lw s9, 32(sp)
+    lw s10, 36(sp)
+    lw s11, 40(sp)
+    addi sp, sp ,-44
+    ret
+
 
 main_exit:
     #/* Simulation End */
@@ -572,8 +720,9 @@ main_exit:
     addi sp, sp, 4
     
     # halt the cpu
-    jal x0 halt
-
-halt:
-	ret
+    hcf
+    hcf
+    hcf
+    hcf
+    hcf
 	
