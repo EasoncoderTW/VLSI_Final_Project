40211d1225063e34193d07bc95f17189b68d5ec6
Easoncoder <easonyehmobile1@gmail.com>
2023-01-05 22:30:28 +0800

    Fix BranchComp and LD Filter

--- src/CPU_Components/Datapath/BranchComp.v
+++ src/CPU_Components/Datapath/BranchComp.v
@@ -8,10 +8,10 @@ module BranchComp (
 
 wire lt_wire;
 wire ltu_wire;
-wire [31:0]sub_wire;
-assign sub_wire = src1 - src2;
+wire [32:0]sub_wire;
+assign sub_wire = {1'b0,src1} - {1'b0,src2};
 
-assign ltu_wire  = (sub_wire[31] == 1'b1)? 1'b1: 1'b0;
+assign ltu_wire  = (sub_wire[32] == 1'b1)? 1'b1: 1'b0;
 assign lt_wire   = ((src1[31] ^ src2[31])==1'b0)? ltu_wire: ~ltu_wire;
 
 assign BrEq   = (sub_wire == 32'd0)? 1'b1: 1'b0;
--- src/CPU_Components/Datapath/LD_Filter.v
+++ src/CPU_Components/Datapath/LD_Filter.v
@@ -3,18 +3,14 @@
 module LD_Filter (
     input [2:0] width,
     input [31:0] ld_data,
-    output reg [31:0] ld_data_f
+    output wire [31:0] ld_data_f
 );
 
-always @(*) begin
-    case(width)
-    `Byte:    ld_data_f = {{24{ld_data[7]}}, ld_data[7:0]};
-    `Half:    ld_data_f = {{16{ld_data[15]}}, ld_data[15:0]};
-    `Word:    ld_data_f = ld_data;
-    `UByte:   ld_data_f = {24'd0, ld_data[7:0]};
-    `UHalf:   ld_data_f = {16'd0, ld_data[15:0]};
-    default: ld_data_f = 32'd0;
-    endcase
-end
+
+assign ld_data_f = (width == `Byte)? {{24{ld_data[7]}}, ld_data[7:0]}:
+                   (width == `Half)? {{16{ld_data[15]}}, ld_data[15:0]}:
+                   (width == `Word)? ld_data:
+                   (width == `UByte)? {24'd0, ld_data[7:0]}:
+                   (width == `UHalf)? {16'd0, ld_data[15:0]}:32'd0;
 
 endmodule
